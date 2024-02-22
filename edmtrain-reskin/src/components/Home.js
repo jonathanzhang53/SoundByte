67702354f8031dee3faba4a85524cc3afd73e4ab
Cameronketchem <cameronketchem@gmail.com>
2024-02-21 20:29:08 -0500

    filters map markers based on city/date

--- edmtrain-reskin/src/components/Home.js
+++ edmtrain-reskin/src/components/Home.js
@@ -7,42 +7,48 @@ import Sidebar from './Sidebar';
 
 function Home({events}) {
 
+  //needs an initial coordinate to set the start as or else map doesn't generate
+  //this is set to Gainesville's lat and long for rn
   const positions = [
     [29.6520,-82.3250]
   ];
 
-  // Fetch the first 10 events
-  if(events.length >0){
-    const first10 = events.slice(0, 10);
+
+  const [searchDates, setDates] = useState('');
+  const [searchLocation, setLocation] = useState('');
+
+  //filters based on search criteria
+  const filteredEvents = events.filter(event => {
+    const matchDates = !searchDates || event.date.includes(searchDates);
+    const matchLocation = !searchLocation || event.venue.location.includes(searchLocation);
+    return matchDates && matchLocation;
+  });
+
+  //Cuts off filtered markers at 500
+  const first10 = filteredEvents.slice(0, 500);
 
   // Extract latitude and longitude coordinates into positions array
-
-  const filteredEvents = events.filter(event => {
-    const matchLocation = event.venue.location.includes("Gainesville");
-    return matchLocation;
-  });
-  
-   first10.forEach(event => {
+  first10.forEach(event => {
     positions.push([event.venue.latitude, event.venue.longitude]);
   });
-    console.log(first10);
-    console.log(positions[0]);
-  }
 
-  const [filterDates, setFilterDates] = useState('');
-  const [filterEDM, setFilterEDM] = useState('');
-  const [filterNonEDM, setFilterNonEDM] = useState('');
 
   return (
     <div style={{ height: '100vh', display: 'flex', flexDirection: 'column', backgroundColor: '#9E6060', paddingTop: '50px' }}>
-      <FilterBar
-        filterDates={filterDates}
-        setFilterDates={setFilterDates}
-        filterEDM={filterEDM}
-        setFilterEDM={setFilterEDM}
-        filterNonEDM={filterNonEDM}
-        setFilterNonEDM={setFilterNonEDM}
-      />
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '10px', width: '65%' }}>
+   <input
+      type="text"
+      placeholder="Search Date (MM-DD-YYYY)"
+      value={searchDates}
+      onChange={e => setDates(e.target.value)}
+      style={{ width: '30%', padding: '5px', marginLeft: '10px' }} />
+    <input
+        type="text"
+        placeholder="Search by City"
+        value={searchLocation}
+        onChange={e => setLocation(e.target.value)}
+        style={{ width: '30%', padding: '5px' }} />
+  </div>
 
       <EventMap positions={positions} />
     
