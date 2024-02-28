d966faaf62a46e8957ceda9b44f2aa3066a537bd
jonathanzhang53 <jonathanzhang53@gmail.com>
2024-02-27 21:11:51 -0500

    datepicker

--- edmtrain-reskin/src/components/Searchbar.js
+++ edmtrain-reskin/src/components/Searchbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function Searchbar({ searchDates, setSearchDates, searchLocation, setSearchLocation }) {
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '10px', width: '65%' }}>
-      <input
-        type="text"
-        placeholder="Search Date (MM-DD-YYYY)"
-        value={searchDates}
-        onChange={e => setSearchDates(e.target.value)}
+    <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '10px', width: '65%', zIndex: 1000 }}>
+      <DatePicker
+        selected={searchDates}
+        onChange={date => setSearchDates(date)}
+        dateFormat="yyyy-MM-dd"
+        placeholderText="Search Date (YYYY-MM-DD)"
         style={{ width: '30%', padding: '5px', marginLeft: '10px' }}
       />
       <input
