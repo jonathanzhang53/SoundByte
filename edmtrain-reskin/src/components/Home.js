e580476dc61f480ecc08bc037d04c8cfdfdc5123
Hannah Hardy <hannahohardy@gmail.com>
2024-02-27 22:43:23 -0500

    sidebar ma gawd

--- edmtrain-reskin/src/components/Home.js
+++ edmtrain-reskin/src/components/Home.js
@@ -15,22 +15,30 @@ function Home({events}) {
     return matchDates && matchLocation;
   });
 
-  // Cuts off filtered markers at 500 (TEMPORARY)
-  const first10 = filteredEvents.slice(0, 500);
+  // Cuts off filtered markers at 500
+  const first10 = filteredEvents.slice(0, 100);
 
   return (
-    <div style={{ height: '100vh', display: 'flex', flexDirection: 'column', backgroundColor: '#9E6060', paddingTop: '50px' }}>
-      <Searchbar
-        searchDates={searchDates}
-        setSearchDates={setDates}
-        searchLocation={searchLocation}
-        setSearchLocation={setLocation}
-      />
-      
-      <EventMap filteredEvents={first10} />
-    
-      {/* FIXME: sidebar overlay map */}
-      {/* <Sidebar events={events} /> */}
+    <div style={{ height: '100vh', display: 'flex', backgroundColor: '#9E6060', paddingTop: '50px' }}>
+      {/* Map container */}
+      <div style={{ flex: 1 }}>
+        <Searchbar
+          searchDates={searchDates}
+          setSearchDates={setDates}
+          searchLocation={searchLocation}
+          setSearchLocation={setLocation}
+        />
+        
+        {/* EventMap with adjusted width */}
+        <div style={{ width: '100%', height: '100%', zIndex: 1 }}>
+          <EventMap filteredEvents={first10} />
+        </div>
+      </div>
+
+      {/* Sidebar with adjusted width */}
+      <div style={{ width: '20%', backgroundColor: '#FFFFFF', padding: '5px', overflowY: 'auto', zIndex: 2 }}>
+        <Sidebar events={first10} />
+      </div>
     </div>
   );
 }
