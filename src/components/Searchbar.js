193938545996b850c83caaa9dd2067ce2146353e
Courtney Giang <courtneygiang@gmail.com>
2024-03-05 22:42:30 -0500

    Display search bar after city selection

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import useFetchCities from '../hooks/useFetchCities';
+import Sidebar from './Sidebar'; // Import Sidebar component
 
 import 'react-datepicker/dist/react-datepicker.css';
 
-function Searchbar({ searchStart,  setStartDate, searchEnd,  setEndDate, searchLocation, setSearchLocation, setMapCenter }) {
+function Searchbar({ searchStart, setStartDate, searchEnd, setEndDate, searchLocation, setSearchLocation, setMapCenter }) {
   const cities = useFetchCities();
-  
+  const [showSidebar, setShowSidebar] = useState(false); // State to manage sidebar visibility
+
+  // Function to handle city selection
+  const handleCitySelection = (e) => {
+    const [name, lat, lon] = e.target.value.split(",");
+    setSearchLocation(name);
+    setMapCenter({ lat: parseFloat(lat), lon: parseFloat(lon) });
+    setShowSidebar(true); // Show the sidebar when city is selected
+  }
+
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '10px', width: '65%', zIndex: 1000 , marginLeft: '100px' }}>
       {/* Start Date Picker */}
@@ -30,11 +40,7 @@ function Searchbar({ searchStart,  setStartDate, searchEnd,  setEndDate, searchL
       {/* City Picker */}
       <select
         value={searchLocation}
-        onChange={e => {
-          const [name, lat, lon] = e.target.value.split(",");
-          setSearchLocation(name);
-          setMapCenter({ lat: parseFloat(lat), lon: parseFloat(lon) });
-        }}
+        onChange={handleCitySelection}
         style={{ width: '30%', height: '40px', padding: '5px'}}
       >
         <option value="">Select a City</option>
@@ -42,6 +48,9 @@ function Searchbar({ searchStart,  setStartDate, searchEnd,  setEndDate, searchL
           <option key={index} value={`${city.name},${city.lat},${city.lon}`}>{city.name + ", " + city.country}</option>
         ))}
       </select>
+
+      {/* Render Sidebar if showSidebar is true */}
+      {showSidebar && <Sidebar />}
     </div>
   );
 }
