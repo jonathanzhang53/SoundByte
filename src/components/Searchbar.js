ad1b2f72512807d32e34aaac0d42ac60ea93c9dc
Courtney Giang <courtneygiang@gmail.com>
2024-03-05 23:17:29 -0500

    Fix merge conflict

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -131,7 +131,7 @@ function Searchbar({ searchStart, setStartDate, searchEnd, setEndDate, searchLoc
         )}
       </div>
 
-      {/* Render Sidebar if showSidebar is true */}
+      {/* Check Sidebar if showSidebar is true */}
       {showSidebar && <Sidebar />}
     </div>
   );
