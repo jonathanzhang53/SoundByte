a2377b850ab5e3dca1309549016aad85faca2b62
jonathanzhang53 <jonathanzhang53@gmail.com>
2024-03-19 22:39:09 -0400

    transformEvents extraction + test

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
         '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
     },
     testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
 };
