772c70de211c68dd265e35cad5ce9b7b2dfe5cf0
Jonathan Zhang <41968151+jonathanzhang53@users.noreply.github.com>
2024-03-26 20:51:17 -0400

    consolidate contexts (#61)

--- src/contexts/EventsContext.js
+++ src/contexts/EventsContext.js
@@ -1,5 +1,47 @@
-import { createContext } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import { transformEvents } from '../assets/transformEvents';
 
 const EventsContext = createContext();
+
+export const EventsProvider = ({ children }) => {
+  const [events, setEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const API_URL = process.env.REACT_APP_API_URL;
+      const API_KEY = process.env.REACT_APP_API_KEY;
+      try {
+        const response = await fetch(`${API_URL}?client=${API_KEY}`);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        if (data.success) {
+          const transformedEvents = transformEvents(data.data);
+          setEvents(transformedEvents);
+        } else {
+          setError('API request failed: ' + data.message);
+        }
+      } catch (error) {
+        setError('Error fetching data: ' + error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const value = { events, isLoading, error };
+
+  return (
+    <EventsContext.Provider value={value}>
+      {children}
+    </EventsContext.Provider>
+  );
+};
 
 export default EventsContext;
