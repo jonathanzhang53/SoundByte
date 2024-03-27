ee1192f12815b8203444b19b3d63604cd50bbe12
jonathanzhang53 <jonathanzhang53@gmail.com>
2024-03-26 23:34:59 -0400

    lol FAK

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,19 @@
 module.exports = {
+    clearMocks: true,
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+        '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
     },
+
+    transform: {
+        '^.+\\.[t|j]sx?$': 'babel-jest',
+    },
+
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+
+    collectCoverage: true,
+    collectCoverageFrom: ["src/**/*.{js,jsx}", "!src/index.js", "!src/reportWebVitals.js", "!src/setupTests.js", "!src/App.js"],
+
+    verbose: true,
 };
